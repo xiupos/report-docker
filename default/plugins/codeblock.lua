fe067353d7a4a6902fc8896fc5d0a1ef004edd3e
xiupos <me@xiupos.net>
2024-04-11 20:45:29 +0900

    update codeblock.lua

--- default/plugins/codeblock.lua
+++ default/plugins/codeblock.lua
@@ -7,14 +7,6 @@ function CodeBlock(el)
       "\\end{figure}",
     }, "\n")
     return pandoc.RawBlock("latex", body)
-  elseif el.classes[1] == "feynmf" then
-    body = table.concat({
-      "\\begin{figure}",
-      "\\centering",
-      el.text,
-      "\\end{figure}",
-    }, "\n")
-    return pandoc.RawBlock("latex", body)
   end
   return el
 end
